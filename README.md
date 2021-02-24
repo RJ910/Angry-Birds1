d765fac5a696d9ee20dee23f178ac82fa3905a18
RJ910 <75748617+RJ910@users.noreply.github.com>
2021-02-24 20:08:26 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Angry-Birds1
+# TopplingBoxes
+Toppling Boxes
---
+++ sketch.js
@@ -0,0 +1,45 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+
+    pig1 = new Pig(810,350);
+    pig2 = new Pig(810,220);
+
+    log1 = new Log(810,260,300,PI/2);
+    log2 = new Log(810,180,300,PI/2);
+
+    bird = new Bird(50,50);
+
+    ground = new Ground(600,height,1200,20)
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    ground.display();
+    pig1.display();
+    pig2.display();
+    log1.display();
+    log2.display();
+    bird.display();
+}
